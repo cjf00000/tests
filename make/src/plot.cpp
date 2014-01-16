2d7def5cafd3eb3b93a70f6f4c84851f7932af7e
Jianfei Chen <cjf00000@gmail.com>
2014-01-16 15:23:51 +0800

    Added make

---
+++ make/src/plot.cpp
@@ -0,0 +1,11 @@
+// Copyright 2014 Jianfei Chen
+#include <cstdio>
+#include "./consts.h"
+
+int main() {
+        for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j)
+                        printf(".");
+                printf("\n");
+        }
+}
